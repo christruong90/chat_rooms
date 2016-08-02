1e1c914a752d0c15a1954faa3b428d666e989492
Chris Truong <chrisanhoi@hotmail.com>
2016-08-02 16:43:44 -0700

    security

--- app/assets/javascripts/chat_rooms.js
+++ app/assets/javascripts/chat_rooms.js
@@ -5,7 +5,8 @@ $(function () {
     // the id/element dom element that will hold remote videos
     remoteVideosEl: 'remoteVideos',
     // immediately ask for camera access
-    autoRequestMedia: true
+    autoRequestMedia: true,
+
   });
 
 
@@ -14,4 +15,18 @@ $(function () {
     // you can name it anything
     webrtc.joinRoom('chris room ');
   });
+
+  $('#pause-button').on('click', function(){
+    if($('#pause-button').text() === 'Pause'){
+      webrtc.stopLocalVideo()
+      $('#pause-button').text('Play')
+      $('#pause-button').removeClass('btn-danger')
+      $('#pause-button').addClass('btn-success')
+    } else {
+      webrtc.startLocalVideo()
+      $('#pause-button').text('Pause')
+      $('#pause-button').removeClass('btn-success')
+      $('#pause-button').addClass('btn-danger')
+    }
+  })
 })
